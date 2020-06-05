87be7e92671430d889a626e149499a5807dd5451
luduyang <luduyang@zto.com>
2020-06-05 10:58:53 +0800

    ....

---
+++ MyPodDemo/Classes/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  MyPodDemo
+//
+//  Created by DuYangLu on 2020/3/17.
+//
+
+import UIKit
+
+class Test: NSObject {
+
+    static func oneTest() {
+
+    }
+}
--- MyPodDemo/Classes/Tool.swift
+++ MyPodDemo/Classes/Tool.swift
@@ -1,6 +1,8 @@
 
 public class Tool {
     public func calculateTotal(a: Int, b: Int) -> Int {
+
+        Test.oneTest()
         return a + b
     }
 }
